e5a84ee4fb3841d8e744b55089483c7c582819aa
Galih <galihakbar.ga91@gmail.com>
2023-05-13 15:51:57 +0700

    implement flask

--- app.py
+++ app.py
@@ -5,30 +5,56 @@ import PIL
 import tensorflow as tf 
 import tensorflow_hub as hub 
 
+# Flask
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+
+# Load Model & labels
 model = "https://tfhub.dev/google/on_device_vision/classifier/landmarks_classifier_asia_V1/1"
 label = "landmarks_classifier_asia_V1_label_map.csv"
 
-imageShape = (321,321,3)
-classifier = tf.keras.Sequential(
-    [hub.KerasLayer(model, input_shape = imageShape, output_key="predictions:logits")])
+@app.route("/")
+def home():
+    return "App is running"
 
-df = pd.read_csv(label)
-print(df)
 
-labelsDict = dict(zip(df.id, df.name))
+@app.route("/upload", methods=["POST"])
+def upload():
+    if "file" not in request.files:
+        return jsonify({"error": "No file found"}), 400
 
-image1 = PIL.Image.open("monas.jpg")
-image1 = image1.resize((321,321))
-print(image1.size)
+    file = request.files["file"]
 
-image1 = np.array(image1)
-image1 = image1/255.0
+    if file.filename == "":
+        return jsonify({"error": "No file selected"}), 400
+    
 
-image1 = image1[np.newaxis]
+    imageShape = (321,321,3)
+    classifier = tf.keras.Sequential(
+        [hub.KerasLayer(model, input_shape = imageShape, output_key="predictions:logits")])
 
-print(image1.shape)
+    df = pd.read_csv(label)
+    print(df)
 
-result = classifier.predict(image1)
-finalResult = np.argmax(result)
+    labelsDict = dict(zip(df.id, df.name))  
 
-print ("The prediction is: " + labelsDict[finalResult])
+    image1 = PIL.Image.open(file)
+    image1 = image1.resize((321,321))
+    print(image1.size)
+
+    image1 = np.array(image1)
+    image1 = image1/255.0
+
+    image1 = image1[np.newaxis]
+
+    print(image1.shape)
+
+    result = classifier.predict(image1)
+    finalResult = np.argmax(result)
+
+    print ("The prediction is: " + labelsDict[finalResult])
+    return jsonify(labelsDict[finalResult])
+
+if __name__ == "__main__":
+    app.run()
